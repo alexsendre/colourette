fc9a1b06b4faa2e0494ed3659770bc94a2670bc9
Alex Sendre <rares.sendre@gmail.com>
2023-11-21 10:57:44 -0600

    Created form outline

---
+++ pages/color/new.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NewColorForm from '../../components/forms/NewColorForm';
+
+export default function AddColorForm() {
+  return (
+    <div>
+      <NewColorForm />
+    </div>
+  );
+}
