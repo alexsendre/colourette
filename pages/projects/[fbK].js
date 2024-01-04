b806402f5d9dadcb104d5592d4228cbd60e6baf8
Alex Sendre <rares.sendre@gmail.com>
2024-01-03 21:47:36 -0600

    disabled certain dependancy messages

--- pages/projects/[fbK].js
+++ pages/projects/[fbK].js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 import { useRouter } from 'next/router';
 import React, { useState, useEffect } from 'react';
